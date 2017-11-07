c23edbf7339558bf9c88d6140fbefeff724dead7
itaouil <itaouil95@gmail.com>
2017-11-07 20:52:31 +0000

    Front end.

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ var bodyParser    = require('body-parser');
 
 // Routes ()
 var index = require('./routes/index');
-var users = require('./routes/users');
+// var users = require('./routes/users');
 
 // Express App
 var app = express();
@@ -27,7 +27,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 // Own routes global middleware
 app.use('/', index);
-app.use('/users', users);
+// app.use('/users', users);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
