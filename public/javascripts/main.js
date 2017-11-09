2fd237d74d477634b07955daf47230f570597d78
itaouil <itaouil95@gmail.com>
2017-11-09 16:51:40 +0000

    Added openweather api and custom geolocation in own API.

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -1,52 +1,176 @@
-// Run JS when page loaded
-$(function() {
+// Variables 
+const geolocation   = "http://localhost:3000/api/geolocation";
+const weather       = "http://localhost:3000/api/weather";
+const appID         = "&APPID=624a5719fc88bdf6359cecd6a3e7de74";
+var units           = "&units=metric";
+var wTemp;
+var fTemp           = [];
+var unit            = "celsius";
 
-    // Storing cities
-    const cities = [];
+// Display local weather
+$(document).ready(function() {
+  
+    // Get geolocation from
+    // own web service and
+    // present result in the
+    // DOM
+    fetch(geolocation)
+      .then(res => res.json())
+      .then(location => {
+        // console.log("Received: ", location.city);
+        // Get weather for location
+        getWeather(location.city);
+        console.log(location.city);
+  
+      });
+  
+  });
 
-    // DOM elememnts used for data injection
-    const searchInput = document.querySelector('.search');
-    const suggestions = document.querySelector('.suggestions');
+// Get weather by calling own
+// web service (composition)
+function getWeather(city) {
 
-    // Endpoint used to retrieve list of cities (world)
-    const endpoint = 'http://localhost:3000/api/cities';
-    
-    // Fetch data
-    // and use the spread operator
-    // to deconstruct the data fetched
-    fetch(endpoint)
-        .then(response => response.json())
-        .then(data => cities.push(...data));
+  // Parse respone to json
+  // and display weathe to
+  // JSON
+  fetch(`${weather}?city=${city}`)
+    .then(resp => resp.json())
+    .then(weather => {
+      render(weather);
+    });
 
-    // Find matching cities
-    function findMatches(cityToMatch, cities) {
-        const regex = new RegExp(cityToMatch, 'gi');
-        return cities.filter(city => {
-            return city.city.match(regex)
-        });
-    }
+}
 
-    // Render data
-    function renderData() {
+// Displays data in the
+// DOM content (web page)
+function render(wData) {
+  //primary weather information
+  $("#location").text(wData.name + ", " + wData.sys.country);
+  var imgsrc = "https://openweathermap.org/img/w/" + wData.weather[0].icon + ".png";
+  $("#icon").html('<img src="' + imgsrc + '" class=".wIcon" alt="w-icon">');
+  wTemp = Math.round(wData.main.temp);
+  $("#temp").text(Math.round(wTemp));
+  $("#mainWeather").text(wData.weather[0].main);
+  $("#tempMax").text(wData.main.temp_max);
+  $("#tempMin").text(wData.main.temp_min);
+  //secondary weather information
+  $("#day").text(dayOfWeek());
+  $("#time").text(currentTime());
+  $("#descWeather").text(wData.weather[0].description);
+  $("#wind").text("Wind: " + wData.wind.speed);
+  $("#humidity").text("Humidity: " + wData.main.humidity + "%");
+}
 
-        const matches = findMatches(this.value, cities).slice(1,10);
-        const html = matches.map(match => {
-            const regex = new RegExp(this.value, 'gi');
-            const cityName = match.city.replace(regex, `<span class='hl'>${this.value}</span>`);
-            // const stateName = match.state.recity(regex, `<span class='hl'>${this.value}</span>`);
-            // const population = numberWithCommas(match.population);
-            return `
-                <li>
-                    <span>${cityName}</span>
-                </li>
-            `;
-        }).join('');
+function displayForecast(fData) {
+  var i = 8; //Used to iterate through JSON data
 
-        suggestions.innerHTML = html;
+  $(".fDay").each(function() {
+    $(this).text(fData.list[i].dt_txt);
+    i += 8;
+  });
+  i = 8;
+  $(".fIcon").each(function() {
+    var imgsrc =
+      "https://openweathermap.org/img/w/" +
+      fData.list[i].weather[0].icon +
+      ".png";
+    //console.log(imgsrc);
+    $(this).html('<img src="' + imgsrc + '" alt="f-icon">');
+    i += 8;
+  });
+  i = 8;
+  $(".fDesc").each(function() {
+    $(this).text(fData.list[i].weather[0].main);
+    i += 8;
+  });
+  i = 8;
+  $(".fTemp").each(function() {
+    fTemp.push(Math.round(fData.list[i].main.temp));
+    //console.log(fTemp);
+    $(this).text(Math.round(fData.list[i].main.temp) + " C");
+    i += 8;
+  });
+  i = 8;
+}
 
-    }
+function dayOfWeek() {
+  var dt = new Date();
+  //console.log(dt);
+  var today = dt.getDay();
+  var days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday"
+  ];
+  return days[today];
+}
 
-    // Input event listening
-    searchInput.addEventListener('keyup', renderData);
+function currentTime() {
+  var now = new Date();
+  var hours = now.getHours();
+  var mins = now.getMinutes();
+  var ampm = "";
+  //logic to convert 24hr format to 12hr and get am/pm correctly
+  if (hours < 12) ampm = "am";
+  else if (hours == 00) {
+    ampm = "am";
+    hours = 12;
+  } else if (hours == 12) ampm = "pm";
+  else {
+    ampm = "pm";
+    hours = hours - 12;
+  }
+  //logic to show minutes as 2 digits even when minutes is lesser than 10
+  mins = "0" + mins; //adds a string '0' to mins value and converts the whole mins to string. ex: 4 becomes 04, 15 becomes 015.
+  mins = mins.slice(-2); //saves mins string as a slice of last 2 characters only. ex: 04 becomes 04, 015 becomes 15
+  var time = "";
+  time = hours + ":" + mins + ampm;
+  return time;
+}
 
+$("#cf").click(function(e) {
+  e.preventDefault();
+  if (unit == "celsius") {
+    unit = "farenheit";
+    wTemp = wTemp * 1.8 + 32;
+    $(".fTemp").each(function(index) {
+      fTemp[index] = fTemp[index] * 1.8 + 32;
+      $(this).text(Math.round(fTemp[index]) + " F");
+    });
+    $("#f")
+      .addClass("selected")
+      .removeClass("unselected");
+    $("#c")
+      .removeClass("selected")
+      .addClass("unselected");
+    $("#temp").text(Math.round(wTemp));
+  } else {
+    unit = "celsius";
+    wTemp = (wTemp - 32) / 1.8;
+    $(".fTemp").each(function(index) {
+      fTemp[index] = (fTemp[index] - 32) / 1.8;
+      $(this).text(Math.round(fTemp[index]) + " C");
+    });
+    $("#c")
+      .addClass("selected")
+      .removeClass("unselected");
+    $("#f")
+      .removeClass("selected")
+      .addClass("unselected");
+    $("#temp").text(Math.round(wTemp));
+  }
+});
+
+$("#goButton").click(function() {
+  if ($("#userInput").val() === "") {
+    alert("Please enter a location.");
+  } else {
+    userRegion = $("#userInput").val();
+    getWeather(userRegion);
+    getForecast(userRegion);
+  }
 });
--- routes/api.js
+++ routes/api.js
@@ -3,22 +3,109 @@ var router = express.Router();
 
 // File reader modules
 var fs = require('fs');
-var path = require("path");
+var path = require('path');
 
-// Sends back list of cities as a JSON
-router.get('/cities', (req, res, next) => {
+// HTTPS modules
+var https = require('https');
 
-  // Read json file in data folder (city.list.json)
-  fs.readFile(path.join(__dirname, '..', 'data', 'city.list.json'), 'utf8', (err, data) => {
+// Get geolocation of the machine
+router.get('/geolocation', (req, res, next) => {
 
-    if (err) {
-      return console.log("Error while reading json file: " + err);
-    }
+    // Hit json file on the web and send it
+    // to the client requesting it
+    https.get('https://ipinfo.io/json', (resp) => {
 
-    res.json(JSON.parse(data));
+      let data = '';
 
-  });
+      // Store chunk of data received
+      resp.on("data", (chunk) => {
+        data += chunk;
+      });
 
+      // Parse response and send back
+      resp.on('end', () => {
+        res.json(JSON.parse(data.trim()));
+      });
+
+      // Catch error
+      resp.on('error', (err) => {
+        res.json({});
+        console.log("Error: " + err.message);
+      });
+
+    });
+  
 });
+
+// Get weather
+router.get('/weather', (req, res, next) => {
+
+      // API info required
+      const url = 'https://api.openweathermap.org/data/2.5/weather?q=';
+      const key = '&appid=624a5719fc88bdf6359cecd6a3e7de74';
+  
+      console.log(req.query.city);
+      
+      // Make request to openweather with
+      // requested city
+      https.get(`${url}${req.query.city}${key}`, (resp) => {
+  
+        let data = '';
+  
+        // Store chunk of data received
+        resp.on("data", (chunk) => {
+          data += chunk;
+        });
+  
+        // Parse response and send back
+        resp.on('end', () => {
+          res.json(JSON.parse(data.trim()));
+        });
+  
+        // Catch error
+        resp.on('error', (err) => {
+          res.json({});
+          console.log("Error: " + err.message);
+        });
+  
+      });
+    
+});
+
+// Get photo
+router.get('/photo', (req, res, next) => {
+  
+        // API info required
+        const url_first   = 'https://api.flickr.com/services/rest/?method=flickr.photos.search&api_key=4ccc692bea7335112f0e94ce94676836';
+        const ulr_second  = '&format=json&nojsoncallback=1&auth_token=72157666113847389-8e8886ebc2403c3b&api_sig=a5b773186f6fc2d80f212a10c7a105e4';
+
+        console.log("Latitude: ", req.query.lat);
+        console.log("Latitude: ", req.query.lon);
+
+        // Make request to openweather with
+        // requested city
+        https.get(`${url_first}&lat=${req.query.lat}&lon=${req.query.lon}${ulr_second}`, (resp) => {
+    
+          let data = '';
+    
+          // Store chunk of data received
+          resp.on("data", (chunk) => {
+            data += chunk;
+          });
+    
+          // Parse response and send back
+          resp.on('end', () => {
+            res.json(JSON.parse(data.trim()));
+          });
+    
+          // Catch error
+          resp.on('error', (err) => {
+            res.json({});
+            console.log("Error: " + err.message);
+          });
+    
+        });
+      
+  });
 
 module.exports = router;
